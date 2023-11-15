fc8e8b98746c6bf16c41fa6e4ecc7dcdaaefe5c4
LTeixeira4909 <ipadljteixeira@gmail.com>
2023-11-15 07:56:24 -0500

    state changes

--- Shooter HAAS/src/main/java/frc/robot/Robot.java
+++ Shooter HAAS/src/main/java/frc/robot/Robot.java
@@ -6,21 +6,37 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.simulation.XboxControllerSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.ShooterConstants;
+import frc.robot.Shooter.shooterStates;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  CommandXboxController controller = new CommandXboxController(Constants.DRIVER_JOYSTICK_PORT);
-
+  final private CommandXboxController controller = new CommandXboxController(Constants.DRIVER_JOYSTICK_PORT);
+  private final Shooter m_Shooter;
   private RobotContainer m_robotContainer;
+
+  public Robot() {
+    m_Shooter = new Shooter(0, 1, 2, 3, 4);
+
+  }
 
   private void configureBindings() {
     Shooter shooter = new Shooter(1, 2, 3, 4, 5);
     controller.rightTrigger().onTrue(new InstantCommand(() -> shooter.shoot()));
+    controller.rightBumper().onTrue(new InstantCommand(() -> shooter.flyWheeleStop()));
+
+    controller.a().onTrue(new InstantCommand(() -> shooter.setshooterState(shooterStates.SHOOTER_HIGH)));
+    controller.x().onTrue(new InstantCommand(() -> shooter.setshooterState(shooterStates.SHOOTER_MID)));
+    controller.y().onTrue(new InstantCommand(() -> shooter.setshooterState(shooterStates.SHOOTER_LOW)));
+    controller.b().onTrue(new InstantCommand(() -> shooter.setshooterState(shooterStates.SHOOTER_RETRACTED)));
+    // controller.rightTrigger().onTrue(new InstantCommand(m_Shooter::shoot));
+    // controller.b().onTrue(new InstantCommand(m_Shooter::flyWheeleStop));
     shooter.getCurrentCommand();
   }
 
@@ -33,6 +49,8 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    SmartDashboard.putString("Command",
+        m_Shooter.getCurrentCommand() != null ? m_Shooter.getCurrentCommand().toString() : "null");
   }
 
   @Override
--- Shooter HAAS/src/main/java/frc/robot/Shooter.java
+++ Shooter HAAS/src/main/java/frc/robot/Shooter.java
@@ -5,9 +5,30 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
     private CANSparkMax motor1, motor2, motor3, motor4, motor5;
+    private shooterStates m_angleSetpoint;
+
+    private shooterStates m_currentState, m_lastState;
+
+    public enum shooterStates {
+        SHOOTER_RETRACTED("Retracted"),
+        SHOOTER_LOW("Low"),
+        SHOOTER_MID("Mid"),
+        SHOOTER_HIGH("High");
+
+        String stateName;
+
+        private shooterStates(String name) {
+            this.stateName = name;
+        }
+
+        public String toString() {
+            return stateName;
+        }
+    }
 
     public Shooter(int motor1ID, int motor2ID, int motor3ID, int motor4ID, int motor5ID) {
         motor1 = new CANSparkMax(motor1ID, MotorType.kBrushless);
@@ -15,14 +36,55 @@ public class Shooter extends SubsystemBase {
         motor3 = new CANSparkMax(motor3ID, MotorType.kBrushless);
         motor4 = new CANSparkMax(motor4ID, MotorType.kBrushless);
         motor5 = new CANSparkMax(motor5ID, MotorType.kBrushless);
+        m_currentState = shooterStates.SHOOTER_RETRACTED;
+    }
+
+    @Override
+    public void periodic() {
+        stateMachine();
+    }
+
+    private void stateMachine() {
+
+        if (m_currentState.equals(m_lastState)) {
+            return;
+        }
+
+        m_lastState = m_currentState;
+        switch (m_currentState) {
+            case SHOOTER_HIGH:
+                m_angleSetpoint = shooterStates.SHOOTER_HIGH;
+                break;
+            case SHOOTER_MID:
+                m_angleSetpoint = shooterStates.SHOOTER_MID;
+                break;
+            case SHOOTER_LOW:
+                m_angleSetpoint = shooterStates.SHOOTER_LOW;
+                break;
+            case SHOOTER_RETRACTED:
+                m_angleSetpoint = shooterStates.SHOOTER_RETRACTED;
+                break;
+
+        }
+    }
+
+    public void setshooterState(shooterStates state) {
+        m_currentState = state;
     }
 
     public void shoot() {
-        motor1.set(.1);
-        motor2.set(.1);
-        motor3.set(.1);
-        motor4.set(.1);
-        motor5.set(.1);
+        motor1.set(ShooterConstants.FLYWHEELESPEED);
+        motor2.set(ShooterConstants.FLYWHEELESPEED);
+        motor3.set(ShooterConstants.FLYWHEELESPEED);
+        motor4.set(ShooterConstants.SPEED);
+        motor5.set(ShooterConstants.SPEED);
+    }
+
+    public void flyWheeleStop() {
+        motor1.set(ShooterConstants.STOP);
+        motor2.set(ShooterConstants.STOP);
+        motor3.set(ShooterConstants.STOP);
+
     }
 
 }
